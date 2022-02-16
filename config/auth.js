5f5a01558c2edc3f391c64c91b01a4e7fc56ae0d
Chandrakantgit <chandrakhannade@gmail.com>
2022-02-16 15:41:44 +0530

    added authorization of server

---
+++ config/auth.js
@@ -0,0 +1,11 @@
+var authToken = "something";
+
+module.exports = {
+  checkAuth: function (authorization) {
+    if ((authorization = authToken)) {
+      return 1;
+    } else {
+      return 0;
+    }
+  },
+};
--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const express = require("express");
 const port = process.env.port || 3000;
+const mongoose = require("mongoose");
+const config = require("./creds").creds;
 const app = express();
+const userRoutes = require("");
+const auth = require("./creds").auth;
+
+mongoose.connect(config.dbLink);
+
+mongoose.connection.on("connected", () => {
+  console.log("Connected to MongoDB");
+});
+mongoose.connection.on("error", (error) => {
+  console.log(error);
+});
 
 app.use(function (req, res, next) {
   res.setHeader("Access-Control-Allow-Origin", "z");
@@ -9,7 +22,17 @@ app.use(function (req, res, next) {
     "Access-Control-Allow-Headers",
     "authorization,userauth,Origin, X-Requested-With, Content-Type, Accept"
   );
+
+  if (auth.checkAuth(req.headers.authorization != 1){
+    res.status(401);
+    res.json({
+      status : "Forbidden",
+      message: "Authorization Missing"
+    })
+  }
 });
+
+app.use("/api_v1/user", userRoutes);
 
 app.listen(port, function () {
   console.log("app is listing on " + port);
